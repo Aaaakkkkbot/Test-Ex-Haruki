6fb9d591326e23679d2590b69677ca017970c491
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2023-03-26 08:27:57 +0530

    Update telethonbasics.py

--- MitsuriRobot/services/telethonbasics.py
+++ MitsuriRobot/services/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from DestinyBot.utils.pluginhelper import runcmd
+from MitsuriRobot.utils.pluginhelper import runcmd
 
 
 async def convert_to_image(event, borg):
