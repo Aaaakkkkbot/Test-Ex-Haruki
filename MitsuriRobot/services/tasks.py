2df349d29cbd213617292312cedcbbfaafba2e25
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2023-03-26 08:27:35 +0530

    Update tasks.py

--- MitsuriRobot/services/tasks.py
+++ MitsuriRobot/services/tasks.py
@@ -2,8 +2,8 @@ from asyncio import Lock, create_task
 from time import time
 from pyrogram import filters
 from pyrogram.types import Message
-from DestinyBot import BOT_ID, BOT_USERNAME, DEV_USERS
-from DestinyBot.services.sections import bold, section, w
+from MitsuriRobot import BOT_ID, BOT_USERNAME, DEV_USERS
+from MitsuriRobot.services.sections import bold, section, w
 tasks = {}
 TASKS_LOCK = Lock()
 arrow = lambda x: (x.text if x else "") + "\n`→`"
