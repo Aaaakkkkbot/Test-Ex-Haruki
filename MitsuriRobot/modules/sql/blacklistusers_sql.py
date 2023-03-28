6814016efa3f45a84e1182bd30e8ef2f4cbbd154
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2023-03-28 11:23:03 +0530

    Update blacklistusers_sql.py

--- MitsuriRobot/modules/sql/blacklistusers_sql.py
+++ MitsuriRobot/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from MItsuriRobot.modules.sql import BASE, SESSION
+from MitsuriRobot.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
